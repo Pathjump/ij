9625cc8be87c8d3e4c455ac60340c105a712a490
Mahmoud <mahmoud@objects.ws>
2013-04-24 14:03:38 +0200

    add the new design of the messages pages

--- web/bundles/objectsinternjump/js/companyMessages.js
+++ web/bundles/objectsinternjump/js/companyMessages.js
@@ -1,100 +1,92 @@
-$(document).ready(function(){
-    $(".chzn-select").livequery(function(){
-        $(this).chosen();
+$(document).ready(function() {
+    $(".select-box select").livequery(function() {
+        $(this).selectbox();
     });
-    $('#readAllMessages').livequery(function(){
-        $(this).on('click', function(){
+
+    $('input[type="checkbox"]').livequery(function() {
+        $(this).ezMark();
+    });
+
+    $('#readAllMessages').livequery(function() {
+        $(this).on('click', function() {
             confirmSubmit('readAllMessages');
         });
     });
-    $('#deleteAllMessages').livequery(function(){
-        $(this).on('click', function(){
+    $('#deleteAllMessages').livequery(function() {
+        $(this).on('click', function() {
             confirmSubmit('deleteAllMessages');
         });
     });
-    setInterval(function(){
-        //Loader Script
-        $('#loading').width($('#loading').parent().width());
-        $('#loading').height($('#loading').parent().height());
-    }, 500);
 });
 
-function changeMessagesSelection(element) {
-    if(element.checked) {
-        $('.companyMessage').each(function() {
-            $(this).attr('checked', 'checked');
-        });
-    } else {
-        $('.companyMessage').each(function() {
-            $(this).removeAttr('checked');
-        });
-    }
+function changeMessagesSelection() {
+    $('.companyMessage').click();
 }
 
 function requestData(urlToRequest, divId, completeFunction) {
-    $('#loading').show();
+    $('.loading').show();
     $.ajax({
         url: urlToRequest,
         success: function(msg) {
             $(divId).html(msg);
         },
-        complete: function(msg) {
-            $('#loading').hide();
-            if($.isFunction(completeFunction)){
+        complete: function() {
+            $('.loading').hide();
+            if ($.isFunction(completeFunction)) {
                 completeFunction();
             }
         }
     });
 }
 
-function getTabContents(tabberObject){
+function getTabContents(tabIndex) {
     var urlToRequest = '';
     var divId = '';
     //specify the url to use
-    switch(tabberObject.index){
-        case 0:
-            urlToRequest = companyInboxurl;
-            divId = '#inboxTab';
-            break;
+    switch (tabIndex) {
         case 1:
-            urlToRequest = companyOutboxurl;
-            divId = '#outboxTab';
+            urlToRequest = companyInboxurl;
+            divId = '#tab1';
             break;
         case 2:
-            urlToRequest = companyMessageurl;
-            divId = '#composeTab';
+            urlToRequest = companyOutboxurl;
+            divId = '#tab2';
             break;
+        default:
+            urlToRequest = companyMessageurl;
+            divId = '#tab3';
     }
     requestData(urlToRequest, divId);
 }
 
 function confirmSubmit(attributToAdd) {
-    if($('.companyMessage:checked').length > 0){
-        if(attributToAdd == 'deleteAllMessages'){
+    if ($('.companyMessage:checked').length > 0) {
+        if (attributToAdd === 'deleteAllMessages') {
             jConfirm(
-                'ok',
-                'cancel',
-                'Are you sure you want to Delete ' + $('.companyMessage:checked').length + ' messages',
-                'Warning',
-                function(confirm){
-                    if(confirm){
-                        submitMessagesForm('deleteAllMessages');
+                    'ok',
+                    'cancel',
+                    'Are you sure you want to Delete ' + $('.companyMessage:checked').length + ' messages',
+                    'Warning',
+                    function(confirm) {
+                        if (confirm) {
+                            submitMessagesForm('deleteAllMessages');
+                        }
                     }
-                });
+            );
         } else {
             submitMessagesForm('readAllMessages');
         }
     }
 }
 
-function submitMessagesForm(attributToAdd){
-    $('#loading').show();
+function submitMessagesForm(attributToAdd) {
+    $('.loading').show();
     $.ajax({
         url: $('#companyMessagesForm').attr('action'),
         type: $('#companyMessagesForm').attr('method'),
         data: $('#companyMessagesForm').serialize() + '&' + attributToAdd + '=1',
         complete: function() {
-            if($('#currentBox').val() == 'outbox'){
+            if ($('#currentBox').val() === 'outbox') {
                 window.location = outboxUrl;
             } else {
                 window.location = inboxUrl;
@@ -103,22 +95,23 @@ function submitMessagesForm(attributToAdd){
     });
 }
 
-function confirmDelete(form, url, divId){
+function confirmDelete(form, url, divId) {
     jConfirm(
-        'ok',
-        'cancel',
-        'Are you sure you want to Delete This Message',
-        'Warning',
-        function(confirm){
-            if(confirm){
-                $.ajax({
-                    url: $(form).attr('action'),
-                    type: $(form).attr('method'),
-                    data: $(form).serialize(),
-                    complete: function() {
-                        requestData(url, divId);
-                    }
-                });
+            'ok',
+            'cancel',
+            'Are you sure you want to Delete This Message',
+            'Warning',
+            function(confirm) {
+                if (confirm) {
+                    $.ajax({
+                        url: $(form).attr('action'),
+                        type: $(form).attr('method'),
+                        data: $(form).serialize(),
+                        complete: function() {
+                            requestData(url, divId);
+                        }
+                    });
+                }
             }
-        });
+    );
 }
