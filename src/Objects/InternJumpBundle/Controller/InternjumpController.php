e8d8b660e3f6136d2faa54a47fc9b58753354c76
ola <ola@objects-ola>
2013-05-01 13:01:00 +0200

    [ Start Facebook Separate actions & twigs ] - Add new Base - Add new home twig - Add new Homepage/landing Action + route

--- src/Objects/InternJumpBundle/Controller/InternjumpController.php
+++ src/Objects/InternJumpBundle/Controller/InternjumpController.php
@@ -536,7 +536,7 @@ class InternjumpController extends ObjectsController {
         $founderRepo = $em->getRepository('ObjectsInternJumpBundle:Founder');
         //get all founders
         $founders = $founderRepo->findAll();
-        
+
         return $this->render('ObjectsInternJumpBundle:Internjump:text.html.twig', array(
                     'title' => 'TermsOfUse',
                     'flag' => '',
@@ -839,16 +839,6 @@ class InternjumpController extends ObjectsController {
         $categoryRepo = $em->getRepository('ObjectsInternJumpBundle:CVCategory');
         $internshipRepo = $em->getRepository('ObjectsInternJumpBundle:Internship');
 
-        //check for logedin user or company
-//        if (TRUE === $this->get('security.context')->isGranted('ROLE_NOTACTIVE')) {
-//            //get logedin user objects
-//            $user = $this->get('security.context')->getToken()->getUser();
-//            return $this->redirect($this->generateUrl('student_task', array('loginName' => $user->getLoginName()), TRUE));
-//        } elseif (TRUE === $this->get('security.context')->isGranted('ROLE_NOTACTIVE_COMPANY')) {
-//            //get logedin company objects
-//            $company = $this->get('security.context')->getToken()->getUser();
-//            return $this->redirect($this->generateUrl('internship', array('loginName' => $company->getLoginName()), TRUE));
-//        }
         //get worth users
         $worthUsers = $userRepo->getWorthUsers(3);
         //get featured companies
@@ -877,6 +867,99 @@ class InternjumpController extends ObjectsController {
                     'allCategory' => $allCategory
         ));
     }
+
+
+    /**
+     * @author Ola
+     * Facebook Homepage/Landing action
+     */
+     public function fb_homePageAction() {
+        //get the request object
+        $request = $this->getRequest();
+        //get the session object
+        $session = $request->getSession();
+
+        //variables for auth
+        $redirectFlag = "";
+        $Url = "";
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $companyRepo = $em->getRepository('ObjectsInternJumpBundle:Company');
+        //get home page companies logo
+        $homeCompanies = $companyRepo->findBy(array('isHome' => TRUE));
+
+
+
+            $flag = "facebook";
+            //Yes, inside facebook
+            $facebook = new \Facebook(array(
+                'appId' => '282137608565990',
+                'secret' => 'c0e978a64bc520a8b677dfa9e57f4746',
+            ));
+            // Get User ID
+            $user = $facebook->getUser();
+
+
+            if ($user) {
+
+                if (TRUE === $this->get('security.context')->isGranted('ROLE_NOTACTIVE') || TRUE === $this->get('security.context')->isGranted('ROLE_NOTACTIVE_COMPANY')) {//->getEmail() != $user->
+                    $this->executeLogoutAction();
+                    return $this->redirect($this->generateUrl('site_homepage'));
+                };
+
+
+                try {
+                    // Proceed knowing you have a logged in user who's authenticated.
+                    $graph_url = 'https://graph.facebook.com/me?access_token=' . $facebook->getAccessToken();
+                    $faceUser = json_decode(file_get_contents($graph_url));
+                    $session->set('facebook_user', $faceUser);
+                    $session->set('facebook_short_live_access_token', $facebook->getAccessToken());
+                    $session->set('currentURL', 'http://apps.facebook.com/internjumpnew/');
+                    return $this->redirect($this->generateUrl('facebook_logging', array(), True));
+
+
+//                    /****************************/
+//                    //Get the user then Login now
+//                    /****************************/
+//                    $user1 = $em->getRepository('ObjectsUserBundle:User')->findOneBy(array('email' => $user_profile['email']));
+//
+                } catch (FacebookApiException $e) {
+                    error_log($e);
+                    $user = null;
+                }
+            } else {
+                //not logged in FB user, then GO to fb login;
+                $params = array(
+                    //'scope' => 'read_stream, friends_likes',
+                    'redirect_uri' => 'http://apps.facebook.com/internjumpnew/' // 'http://internjump.com/app_dev.php/'
+                );
+
+                $loginUrl = $facebook->getLoginUrl($params);
+                return $this->redirect($loginUrl);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        /* NOT LOGGED in */
+        return $this->render('ObjectsInternJumpBundle:Internjump:fb_homePage.html.twig', array(
+                    'flag' => $flag,
+                    'reFlag' => $redirectFlag,
+                    'url' => $Url,
+                    'homeCompanies' => $homeCompanies,
+                    'home_page_video_id' => $this->container->getParameter('home_page_video_id')
+        ));
+//        return $this->redirect($this->generateUrl('login', array(), 'true'));
+    }
+
 
     /**
      * @author Ola
